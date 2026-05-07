08a1b4166360e8d87135427c951ef6a1e5c2484e
DarioTapiaPy <dario.tapia215@gmail.com>
2026-05-07 17:05:37 -0400

    Agregué las carpetas de los microservicios

---
+++ Socios/src/test/java/com/GimnasioBerserker/Socios/SociosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GimnasioBerserker.Socios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SociosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
