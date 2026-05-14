7a061893f24b01cb2b80911a5912de4461f9af9e
nicolasmorales <moralesnicolas73@gmail.com>
2026-05-13 23:13:59 -0400

    creacion de todo

---
+++ Comercial/src/main/java/com/GimnasioBerserker/Comercial/controller/ProductoController.java
@@ -0,0 +1,42 @@
+package com.GimnasioBerserker.Comercial.controller;
+
+import com.GimnasioBerserker.Comercial.model.Producto;
+import com.GimnasioBerserker.Comercial.service.ProductoService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@RestController
+@RequestMapping("/productos")
+public class ProductoController {
+    @Autowired
+    private ProductoService productoService;
+
+    @GetMapping
+    public List<Producto> listarProductos(){
+        return productoService.listarProductos();
+    }
+
+    @GetMapping("/{id}")
+    public Producto buscarPorId(@PathVariable Long id){
+        return productoService.findById(id);
+    }
+    @PostMapping
+    public Producto guardarProducto(@Valid @RequestBody Producto producto){
+        return productoService.guardarproducto(producto);
+    }
+
+    @PutMapping("/{id}")
+    public  Producto actualizarProducto(@Valid @RequestBody Producto producto, @PathVariable Long id){
+        return productoService.actualizarproducto(id, producto);
+
+    }
+
+    @DeleteMapping("/{id}")
+    public void eliminarProducto(@PathVariable Long id){
+        productoService.eliminarProducto(id);
+    }
+
+
+}
