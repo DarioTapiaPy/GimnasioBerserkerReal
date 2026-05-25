05c418de9b14dfefb5fb45f5b4a1266743d2e83e
nicolasmorales <moralesnicolas73@gmail.com>
2026-05-25 15:08:41 -0400

    conecte pagos con factura en dto

--- Facturacion/src/main/java/com/GimnasioBerserker/Facturacion/service/PagoService.java
+++ Facturacion/src/main/java/com/GimnasioBerserker/Facturacion/service/PagoService.java
@@ -1,12 +1,17 @@
 package com.GimnasioBerserker.Facturacion.service;
 
+import com.GimnasioBerserker.Facturacion.dto.PagoFacturaDTO;
 import com.GimnasioBerserker.Facturacion.model.Pago;
+import com.GimnasioBerserker.Facturacion.dto.PagoDto;
+import com.GimnasioBerserker.Facturacion.model.Factura;
 
+import com.GimnasioBerserker.Facturacion.repository.FacturaRepository;
 import com.GimnasioBerserker.Facturacion.repository.PagoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PagoService {
@@ -41,5 +46,32 @@ public class PagoService {
     }
     public void eliminarPago(Long id){
         pagoRepository.deleteById(id);
+    }
+
+
+    @Autowired
+    private FacturaRepository facturaRepository;
+    public PagoFacturaDTO obtenerPagoConFactura(Long id){
+        Pago pago = pagoRepository.findById(id).orElse(null);
+
+        if (pago == null){
+            return null;
+        }
+        Factura factura = facturaRepository.findById(pago.getFacturaId()).orElse(null);
+        if (factura == null){
+
+        }
+        PagoFacturaDTO dto = new PagoFacturaDTO();
+        dto.setFacturaId(pago.getId());
+        dto.setFacturaId(pago.getFacturaId());
+        dto.setFechaPago(pago.getFechaPago());
+        dto.setMontoPago(pago.getMontoPago());
+        dto.setMetodoPago(pago.getMetodoPago());
+
+        dto.setIdSocio(factura.getIdSocio());
+        dto.setValor(factura.getValor());
+        dto.setFecha_facturacion(factura.getFecha_facturacion());
+
+        return dto;
     }
 }
