46b33900aff8bc727ac41dc1db7330f4852b05bb
JoseDonosoRivas <josedonoso.iev@gmail.com>
2026-05-24 22:55:45 -0400

    realizacion de configuracion para iniciar la bd y tambien cargue datos iniciales

--- Empleados/src/main/java/com/GimnasioBerserker/Empleados/Config/DataInitializer.java
+++ Empleados/src/main/java/com/GimnasioBerserker/Empleados/Config/DataInitializer.java
@@ -1,4 +1,48 @@
 package com.GimnasioBerserker.Empleados.Config;
 
-public class DataInitializer {
+import com.GimnasioBerserker.Empleados.Model.Empleado;
+import com.GimnasioBerserker.Empleados.Repository.EmpleadoRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class DataInitializer implements CommandLineRunner {
+
+    private final EmpleadoRepository empleadoRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        if (empleadoRepository.count() > 0) {
+            log.info(">>> Empleados ya cargados en BD. Omitiendo inicialización.");
+        } else {
+            log.info(">>> Cargando empleados iniciales en el puerto 8084...");
+
+
+            empleadoRepository.save(new Empleado(
+                    null,
+                    "12345678-9",
+                    "Jose Pérez",
+                    "Entrenador",
+                    "Musculación",
+                    new BigDecimal("850000.00")
+            ));
+
+            empleadoRepository.save(new Empleado(
+                    null,
+                    "18765432-1",
+                    "Laura Méndez",
+                    "Nutricionista",
+                    "Dietética Deportiva",
+                    new BigDecimal("920000.50")
+            ));
+
+            log.info(">>> Se han cargado 2 empleados de prueba correctamente.");
+        }
+    }
 }
