c1604e1b88c529780c4c9d37e2899a60c0795de8
josé <josm.donoso@duocuc.cl>
2026-06-24 14:01:37 -0400

    implementacion real

--- Comercial/src/main/java/com/GimnasioBerserker/Comercial/client/Facturacionclient.java
+++ Comercial/src/main/java/com/GimnasioBerserker/Comercial/client/Facturacionclient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "facturacion-client", url = "http://localhost:8084")
+@FeignClient(name = "facturacion-client", url = "http://facturacion-service:8084")
 public interface Facturacionclient {
 
     @PostMapping("/api/facturacion/facturas")
